35d3dcf995574d166b85b59d0e359e442eee34ce
hecklinderjatin <itsjstar07@gmail.com>
2025-06-14 20:16:05 +0530

    woke

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import StarsBackground from './components/canvas/StarsBackground';
+import ModelCanvas from './components/canvas/ModelCanvas';
+import './App.css'; // Assuming you have some global CSS here
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full h-screen relative overflow-hidden bg-slate-900">
+      {/* StarsBackground will handle its own positioning */}
+      <StarsBackground />
+      {/* ModelCanvas will also handle its own positioning */}
+      <ModelCanvas />
     </div>
   );
 }
