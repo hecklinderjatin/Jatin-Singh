87b830eb0ae9452e9c9b5fee523c2eeece789414
hecklinderjatin <itsjstar07@gmail.com>
2025-06-14 20:26:17 +0530

    htmls

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import StarsBackground from './components/canvas/StarsBackground';
 import ModelCanvas from './components/canvas/ModelCanvas';
 import './App.css'; // Assuming you have some global CSS here
 
 function App() {
   return (
-    <div className="App w-full h-screen relative overflow-hidden bg-slate-900">
-      {/* StarsBackground will handle its own positioning */}
-      <StarsBackground />
-      {/* ModelCanvas will also handle its own positioning */}
-      <ModelCanvas />
-    </div>
+    <Router basename="/Jatin-Singh">
+      <div className="App w-full h-screen relative overflow-hidden bg-slate-900">
+        {/* StarsBackground will handle its own positioning */}
+        <StarsBackground />
+        {/* ModelCanvas will also handle its own positioning */}
+        <ModelCanvas />
+      </div>
+    </Router>
   );
 }
 
