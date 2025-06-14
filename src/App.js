b48904a2ca618e82e4820f23dd57dafece6ef76b
hecklinderjatin <itsjstar07@gmail.com>
2025-06-14 21:43:54 +0530

    fix 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+
 import StarsBackground from './components/canvas/StarsBackground';
 import ModelCanvas from './components/canvas/ModelCanvas';
 import './App.css'; // Assuming you have some global CSS here
 
 function App() {
   return (
-    <Router>
+
       <div className="App w-full h-screen relative overflow-hidden bg-slate-900">
         {/* StarsBackground will handle its own positioning */}
         <StarsBackground />
         {/* ModelCanvas will also handle its own positioning */}
         <ModelCanvas />
       </div>
-    </Router>
   );
 }
 
